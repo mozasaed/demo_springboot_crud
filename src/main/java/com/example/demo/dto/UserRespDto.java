aa1abd8157d06f7e12df282481579bc0daebe133
mozasaed <mozamohammedsaid@gmail.com>
2023-05-04 19:29:11 +0300

    crud demo

--- src/main/java/com/example/demo/dto/UserRespDto.java
+++ src/main/java/com/example/demo/dto/UserRespDto.java
@@ -4,4 +4,10 @@ import lombok.Data;
 
 @Data
 public class UserRespDto {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
 }
